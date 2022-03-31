6196663187e79b47af55a35bca3afa16993b9594
BhupswatiNetwork <bhupswatinetwork@gmail.com>
2022-03-31 16:12:14 +0530

    Changed footer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,7 @@
 import "bootstrap/dist/css/bootstrap.css";
 import Carousel from "react-bootstrap/Carousel";
 import slider1 from "../assets/slide1.png";
-import slider2 from "../assets/slide2.jpeg";
-import slider3 from "../assets/slide3.png";
+import slider2 from "../assets/slider2.png";
 import { Link } from "react-router-dom";
 
 function Home() {
@@ -17,12 +16,6 @@ function Home() {
         </Carousel.Item>
         <Carousel.Item interval={1000} style={{ position: "relative" }}>
           <img className='d-block w-100' src={slider2} alt='Slider Two' />
-          <Link to='/services'>
-            <button className='btn app-btn primary-btn'>Know More</button>
-          </Link>
-        </Carousel.Item>
-        <Carousel.Item interval={1000} style={{ position: "relative" }}>
-          <img className='d-block w-100' src={slider3} alt='Slider Three' />
           <Link to='/services'>
             <button className='btn app-btn primary-btn'>Know More</button>
           </Link>
