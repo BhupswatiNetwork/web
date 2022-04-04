116be01208f38eaf84fce832b29dacac046800c9
BhupswatiNetwork <bhupswatinetwork@gmail.com>
2022-04-04 09:15:36 +0530

    whatsapp chat option added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,40 +1,63 @@
 import fb from "../assets/facebook.svg";
 import linkedin from "../assets/linkedin.svg";
 import twitter from "../assets/twitter.svg";
+import whatsapp from "../assets/whatsapp.svg";
 
 function Footer() {
   return (
-    <div className='footer-container'>
-      <p className='text-xsm'>helpdesk@bhupswatinetwork.in</p>
-      <p className='footer-text'>
-        &copy;{" "}
-        <span className='text-hglt'>Bhupswati Network Private Limited</span>.
-        All rights reserved
-      </p>
-      <div className='social-links'>
-        <div className='icons'>
-          <a
-            href='https://www.facebook.com/BhupswatiNetwork/?msclkid=487b484cafe611ecafaa2da6fa67cfb1'
-            target='_blank'
-            rel='noreferrer'
-          >
-            <img src={fb} alt='facebook logo' />
-          </a>
-        </div>
-        <div className='icons'>
-          <a
-            href='https://in.linkedin.com/in/bhupswati-network-private-limited-366592223?msclkid=f5648b8dafe611ecb71727bfc4756de3'
-            target='_blank'
-            rel='noreferrer'
-          >
-            <img src={linkedin} alt='instagram logo' />
-          </a>
-        </div>
-        <div className='icons'>
-          <img src={twitter} alt='twitter logo' />
+    <>
+      <a
+        style={{
+          position: "fixed",
+          left: "20px",
+          bottom: "20px",
+          zIndex: "15",
+          fontWeight: "600",
+          fontSize: "1.2rem",
+          backgroundColor: "#25D366",
+        }}
+        className='btn whatsapp'
+        href='https://api.whatsapp.com/send?phone=+919540318192'
+        target='_blank'
+        rel='noreferrer'
+      >
+        <span style={{ marginRight: "5px" }}>
+          <img className='icon' src={whatsapp} alt='whatsapp icon' />
+        </span>
+        Chat with us
+      </a>
+      <div className='footer-container'>
+        <p className='text-xsm'>helpdesk@bhupswatinetwork.in</p>
+        <p className='footer-text'>
+          &copy;{" "}
+          <span className='text-hglt'>Bhupswati Network Private Limited</span>.
+          All rights reserved
+        </p>
+        <div className='social-links'>
+          <div className='icons'>
+            <a
+              href='https://www.facebook.com/BhupswatiNetwork/?msclkid=487b484cafe611ecafaa2da6fa67cfb1'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <img src={fb} alt='facebook logo' />
+            </a>
+          </div>
+          <div className='icons'>
+            <a
+              href='https://in.linkedin.com/in/bhupswati-network-private-limited-366592223?msclkid=f5648b8dafe611ecb71727bfc4756de3'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <img src={linkedin} alt='instagram logo' />
+            </a>
+          </div>
+          <div className='icons'>
+            <img src={twitter} alt='twitter logo' />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
